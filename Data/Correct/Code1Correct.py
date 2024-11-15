0099e0a332437eb4215434fcdbe8f6721c59f0fe
Saad Liaquat <25100259@lums.edu.pk>
2024-11-15 20:36:19 +0500

    test executing
    test executing

--- Data/Correct/Code1Correct.py
+++ Data/Correct/Code1Correct.py
@@ -3,17 +3,17 @@
 class Math():
     def __init__(self):
         pass
-    def add(a, b):
+    def add(self,a, b):
         return a + b
 
-    def subtract(a, b):
+    def subtract(self,a, b):
         return a - b
 
-    def multiply(a, b):
+    def multiply(self,a, b):
         return a * b
 
-    def divide(a, b):
+    def divide(self,a, b):
         return a / b
 
-    def power(a, b):
+    def power(self,a, b):
         return a ** b
--- Data/Test/Code1Test.py
+++ Data/Test/Code1Test.py
@@ -3,16 +3,18 @@ import os
 import inspect
 
 
+import os
+import sys
+import inspect
+
 def compare_functions():
     """
     Compares two files containing a Math class:
     - Checks if function names (including __init__) match.
-    - Checks if function implementations match.
+    - Checks if function outputs match when functions are called on instances.
     Returns True if both match; False otherwise.
     """
     try:
-        # test Math class
-        
         correct_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         buggy_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -23,6 +25,7 @@ def compare_functions():
         sys.path.append(buggy_path)
         from Buggy import Code1Buggy as buggy
         sys.path.remove(buggy_path)
+        
         correct_functions = inspect.getmembers(correct.Math, inspect.isfunction)
         buggy_functions = inspect.getmembers(buggy.Math, inspect.isfunction)
 
@@ -30,35 +33,27 @@ def compare_functions():
             print("Different number of functions in Math class.")
             return False
 
-        # Testing all functions
-        
         for i in range(len(correct_functions)):
             if correct_functions[i][0] != buggy_functions[i][0]:
                 print("Function names do not match.")
                 return False
 
-            # Check if implementations match
-            correct_func_code = inspect.getsource(correct_functions[i][1])
-            buggy_func_code = inspect.getsource(buggy_functions[i][1])
+            correct_instance = correct.Math()
+            buggy_instance = buggy.Math()
 
-            # if correct_func_code != buggy_func_code:
-            #     print("Function implementations do not match.")
-            #     return False
-            
-            if correct_functions[i][0] != '__init__':
-                ouput_correct = correct_functions[i][1](2, 3)
-                ouput_buggy = buggy_functions[i][1](2, 3)
-            
-                if ouput_correct != ouput_buggy:
-                    print("Function output does not match.")
+            if correct_functions[i][0] != '__init__':  
+                output_correct = correct_functions[i][1](correct_instance, 2, 3)
+                output_buggy = buggy_functions[i][1](buggy_instance, 2, 3)
+
+                if output_correct != output_buggy:
+                    print(f"Function {correct_functions[i][0]} output does not match.")
                     return False
-            
+
         return True
     
     except Exception as e:
         print(str(e))
         return False
 
-# Run the comparison
 if __name__ == "__main__":
     print(compare_functions())
